804b76dd56c1425087e98df4d7b965711f748fc7
Ekaterina Kulpina <125750430+katrinakulpina@users.noreply.github.com>
2023-04-06 20:59:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import chi2
 
 
 chat_id = 324047628 # Ваш chat ID, не меняйте название переменной
@@ -10,10 +10,9 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    from scipy.stats import chi2
     alpha = 1 - p
     n = len(x)
-    s = 1/n * sum((x - x.mean())**2)
-
-    return n * s / (27 * chi2.ppf(1 - alpha / 2, n)), \
-           n * s / (27 * chi2.ppf(alpha / 2, n))
+    sum_x2 = sum(x**2)
+    q_l = chi2(2*n).ppf(q=alpha/2)
+    q_r = chi2(2*n).ppf(q=1-alpha/2)
+    return np.sqrt(sum_x2 / q_r / 27), np.sqrt(sum_x2 / q_l / 27)
